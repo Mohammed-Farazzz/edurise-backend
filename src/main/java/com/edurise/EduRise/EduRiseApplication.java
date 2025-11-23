4b301f481fda564d7788629a4ed19ae07f37120e
Mohammed-Farazzz <mdfaraz5522@gmail.com>
2025-11-23 22:59:24 +0530

    Update EduRiseApplication.java

--- src/main/java/com/edurise/EduRise/EduRiseApplication.java
+++ src/main/java/com/edurise/EduRise/EduRiseApplication.java
@@ -2,12 +2,15 @@ package com.edurise.EduRise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.edurise.EduRise")
+@EnableJpaRepositories(basePackages = "com.edurise.EduRise.repository")
+@EntityScan(basePackages = "com.edurise.EduRise.model")
 public class EduRiseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EduRiseApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EduRiseApplication.class, args);
+    }
 }
